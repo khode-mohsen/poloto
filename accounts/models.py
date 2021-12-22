da4b2cac0c509a9dab89486080286815d988a675
khode-mohsen <mohsen.shokri.hcs@gmail.com>
2021-12-23 03:10:01 +0330

    login and logout system now in touch

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    content = models.TextField()
+    def __str__(self):
+        return self.title
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+# from .views import login_view
 
-urlpatterns = [
-
-]
+# urlpatterns = [
+#     path('login/', login_view),
+#
+# ]
